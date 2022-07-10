7b91bb050659dc1e70ce9508c512b713aab68499
sai0698 <sairam.kotha@icloud.com>
2022-07-10 16:10:42 +0530

    Initial commit

---
+++ app/src/main/java/com/ksappdev/landscapeandroidmode/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ksappdev.landscapeandroidmode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
